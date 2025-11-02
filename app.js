2d278332685b618b2061ce744259cb0be0c5c020
Shittu Emmanuel <shem4soul@gmail.com>
2025-11-02 17:08:00 +0100

    Add CORS support and update package dependencies

--- app.js
+++ app.js
@@ -14,6 +14,42 @@ app.use((req, res, next) => {
     next();
 });
 
+
+// const rateLimit = require("express-rate-limit");
+
+// const limiter = rateLimit({
+//   windowMs: 15 * 60 * 1000, // 15 minutes
+//   max: 100, // limit each IP to 100 requests
+// });
+
+// app.use(limiter);
+
+
+// const cors = require("cors");
+
+// const allowedOrigins = [
+//   "http://localhost:3000",
+//   "https://scholarguide.onrender.com",
+//   "https://www.scholarguidetech.com",
+//   "http://scholarguidetech.com",
+//   "https://scholarguide-waitlist.vercel.app",
+// ];
+
+// const corsOptions = {
+//   origin: function (origin, callback) {
+//     if (!origin || allowedOrigins.includes(origin)) {
+//       callback(null, true);
+//     } else {
+//       callback(new Error("Not allowed by CORS"));
+//     }
+//   },
+//   methods: ["GET", "POST", "PUT", "PATCH", "DELETE"],
+//   allowedHeaders: ["Content-Type", "Authorization"],
+// };
+
+// app.use(cors(corsOptions));
+
+
 app.use('/feed', feedRoutes);
 
 app.listen(8080);
