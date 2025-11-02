8a1053324cca15f03ea912aada6088475ddb6bf1
Shittu Emmanuel <shem4soul@gmail.com>
2025-11-02 06:05:21 +0100

    Add POST route for creating new posts in feed

--- routes/feed.js
+++ routes/feed.js
@@ -7,5 +7,7 @@ const router = express.Router();
 //GET/ feed/posts
 router.get('/posts', feedController.getPosts);
 
+//Post/ feed/post
+router.post('/post', feedController.createPost);
 
 module.exports = router;
