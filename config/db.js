a55f5abdbdbd1c1cd731d28d2d5618c9b690a525
Shittu Emmanuel <shem4soul@gmail.com>
2025-11-05 08:24:28 +0100

    Refactor MongoDB connection setup by removing deprecated options

--- config/db.js
+++ config/db.js
@@ -2,15 +2,12 @@ const mongoose = require("mongoose");
 
 const connectDB = async () => {
   try {
-    const conn = await mongoose.connect(process.env.MONGO_URI, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-    });
+    const conn = await mongoose.connect(process.env.MONGO_URI);
 
     console.log(`✅ MongoDB Connected: ${conn.connection.host}`);
   } catch (error) {
     console.error(`❌ DB Connection Failed: ${error.message}`);
-    process.exit(1); 
+    process.exit(1); // Stop the app if DB connection fails
   }
 };
 
