b3f2da7dabe6b80407963152f9e5a7de11979403
Shittu Emmanuel <shem4soul@gmail.com>
2025-11-05 01:40:55 +0100

    Add feed validation module for post creation

---
+++ validatiors/feedValidation.js
@@ -0,0 +1,12 @@
+const { body } = require("express-validator");
+
+exports.createPostValidation = [
+  body("title")
+    .trim()
+    .isLength({ min: 5 })
+    .withMessage("Title must be at least 5 characters long"),
+  body("content")
+    .trim()
+    .isLength({ min: 5 })
+    .withMessage("Content must be at least 5 characters long"),
+];
