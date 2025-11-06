a2a952186731c1b1fe2c49ddb15b49ee4faa050f
Shittu Emmanuel <shem4soul@gmail.com>
2025-11-06 19:55:40 +0100

    Add centralized error handling middleware to log errors and return JSON response

--- app.js
+++ app.js
@@ -91,3 +91,11 @@ connectDB()
 
 // ✅ Register routes
 app.use("/feed", feedRoutes);
+
+
+app.use((error, req, res, next) => {
+  console.log(error);
+  const status = error.statusCode || 500;
+  const message = error.message;
+  res.status(status).json({ message: message });
+});
