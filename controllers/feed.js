42952664543eee3eda0d5034230d02adcdeef751
Shittu Emmanuel <shem4soul@gmail.com>
2025-11-06 18:46:41 +0100

    Refactor createPost method to save post data in the database and return the created post

--- controllers/feed.js
+++ controllers/feed.js
@@ -1,6 +1,8 @@
 const  post  = require("../routes/feed");
 const { validationResult } = require("express-validator");
 
+const Post = require("../models/post");
+
 
 exports.getPosts = (req, res, next) => {
     res.status(200).json({
@@ -31,15 +33,22 @@ exports.createPost = (req, res, next) => {
 
     const title = req.body.title;
     const content = req.body.content;
-    //Create post in db
-    res.status(201).json({
+    const post = new Post({
+      title: title,
+      content: content,
+      creator: { name: "Mr shem" },
+    });
+    post
+       .save()
+       .then(result => {
+        console.log(result);
+        res.status(201).json({
         message: 'Post created successfully!',
-        post: { 
-        _id: new Date().toISOString(),
-         title:title,
-         content: content,
-        creator: {name: 'Mr shem'},
-        createdAt: new Date()
-        }
+        post: result
+       });
+    })
+    //Create post in db
+    .catch(err => {
+        console.log(err);
     });
 };
