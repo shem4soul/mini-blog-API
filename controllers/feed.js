7a3c2aebf1c59ad243a796c102b6ab1b9991cd36
Shittu Emmanuel <shem4soul@gmail.com>
2025-11-06 19:53:24 +0100

    Refactor createPost method for improved error handling and code clarity

--- controllers/feed.js
+++ controllers/feed.js
@@ -20,36 +20,33 @@ exports.getPosts = (req, res, next) => {
     });
 };
 
-
 exports.createPost = (req, res, next) => {
-    const errors = validationResult(req);
-    if (!errors.isEmpty()) {
-      return res
-      .status(422)
-      .json({
-      message: 'Validation failed, entered data is incorrect',
-       errors: errors.array() });
-    }
-
-    const title = req.body.title;
-    const content = req.body.content;
-    const post = new Post({
-      title: title,
-      content: content,
-      imageUrl: 'images/hp.jpeg',
-      creator: { name: "Mr shem" },
-    });
-    post
-       .save()
-       .then(result => {
-        console.log(result);
-        res.status(201).json({
-        message: 'Post created successfully!',
-        post: result
-       });
+  const errors = validationResult(req);
+  if (!errors.isEmpty()) {
+    const error = new Error("Validation failed, entered data is incorrect.");
+    error.statusCode = 422;
+    throw error;
+  }
+  const title = req.body.title;
+  const content = req.body.content;
+  const post = new Post({
+    title: title,
+    content: content,
+    imageUrl: "images/duck.jpg",
+    creator: { name: "Maximilian" },
+  });
+  post
+    .save()
+    .then((result) => {
+      res.status(201).json({
+        message: "Post created successfully!",
+        post: result,
+      });
     })
-    //Create post in db
-    .catch(err => {
-        console.log(err);
+    .catch((err) => {
+      if (!err.statusCode) {
+        err.statusCode = 500;
+      }
+      next(err);
     });
 };
