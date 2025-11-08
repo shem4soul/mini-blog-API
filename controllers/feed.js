02c074b9c93c3449398d025c352b591f1d070501
Shittu Emmanuel <shem4soul@gmail.com>
2025-11-08 16:12:16 +0100

    added cloudinary setup

--- controllers/feed.js
+++ controllers/feed.js
@@ -1,6 +1,7 @@
 const { validationResult } = require("express-validator");
 
 const Post = require("../models/post");
+const cloudinary = require('../config/cloudinary'); 
 
 exports.getPosts = (req, res, next) => {
   Post.find()
@@ -17,41 +18,48 @@ exports.getPosts = (req, res, next) => {
     });
 };
 
-exports.createPost = (req, res, next) => {
+exports.createPost = async (req, res, next) => {
   const errors = validationResult(req);
   if (!errors.isEmpty()) {
     const error = new Error("Validation failed, entered data is incorrect.");
     error.statusCode = 422;
     throw error;
-  } 
-    if (!req.file) {
-      const error = new Error('No image provided.');
-      error.statusCode = 422;
-      throw error;
-    }
-  const imageUrl = req.file.path;
-  const title = req.body.title;
-  const content = req.body.content;
-  const post = new Post({
-    title: title,
-    content: content,
-    imageUrl: imageUrl,
-    creator: { name: "shem" },
-  });
-  post
-    .save()
-    .then((result) => {
-      res.status(201).json({
-        message: "Post created successfully!",
-        post: result,
-      });
-    })
-    .catch((err) => {
-      if (!err.statusCode) {
-        err.statusCode = 500;
-      }
-      next(err);
+  }
+
+  if (!req.file) {
+    const error = new Error("No image provided.");
+    error.statusCode = 422;
+    throw error;
+  }
+
+  try {
+    // Upload image to Cloudinary
+    const result = await cloudinary.uploader.upload(req.file.path, {
+      folder: "mini-blog", // optional folder
     });
+
+    const title = req.body.title;
+    const content = req.body.content;
+
+    const post = new Post({
+      title,
+      content,
+      imageUrl: result.secure_url, // Cloudinary URL
+      creator: { name: "shem" },
+    });
+
+    const savedPost = await post.save();
+
+    res.status(201).json({
+      message: "Post created successfully!",
+      post: savedPost,
+    });
+  } catch (err) {
+    if (!err.statusCode) {
+      err.statusCode = 500;
+    }
+    next(err);
+  }
 };
 
 exports.getPost = (req, res, next) => {
