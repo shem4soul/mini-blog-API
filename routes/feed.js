b8744c04f0ec5cd80a154014f5ec2928a6cc973c
Shittu Emmanuel <shem4soul@gmail.com>
2025-11-08 13:19:04 +0100

    fix a bug

--- routes/feed.js
+++ routes/feed.js
@@ -4,13 +4,15 @@ const feedController = require('../controllers/feed');
 
 const { createPostValidation } = require("../validations/feedValidation");
 
+const upload = require("../middlewares/multer");
+
 const router = express.Router();
 
 //GET/ feed/posts
 router.get('/posts', feedController.getPosts);
 
 //Post/ feed/post
-router.post('/post', createPostValidation, feedController.createPost);
+router.post('/post', upload.single('image'), createPostValidation, feedController.createPost);
 
 router.get('/post/:postId', feedController.getPost);
 
