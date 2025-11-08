04d83e30508c04d2e2a939a1641c6d658b6e49f2
Shittu Emmanuel <shem4soul@gmail.com>
2025-11-08 11:09:30 +0100

    Refactor getPosts method for improved readability and consistency

--- controllers/feed.js
+++ controllers/feed.js
@@ -1,17 +1,15 @@
-const  post  = require("../routes/feed");
 const { validationResult } = require("express-validator");
 
 const Post = require("../models/post");
 
-
 exports.getPosts = (req, res, next) => {
-    Post.find()
-    .then(posts => {
-      res 
+  Post.find()
+    .then((posts) => {
+      res
         .status(200)
-        .json({message: 'Fetched posts successfully.', posts: posts});
+        .json({ message: "Fetched posts successfully.", posts: posts });
     })
-    .catch(err => {
+    .catch((err) => {
       if (!err.statusCode) {
         err.statusCode = 500;
       }
@@ -49,7 +47,6 @@ exports.createPost = (req, res, next) => {
       next(err);
     });
 };
-
 
 exports.getPost = (req, res, next) => {
   const postId = req.params.postId;
