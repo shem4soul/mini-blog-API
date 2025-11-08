ba4c77f655c8c681401ba60e174d657145a95d1d
Shittu Emmanuel <shem4soul@gmail.com>
2025-11-08 18:45:46 +0100

    added validation for update post

--- validations/feedValidation.js
+++ validations/feedValidation.js
@@ -10,3 +10,18 @@ exports.createPostValidation = [
     .isLength({ min: 5 })
     .withMessage("Content must be at least 5 characters long"),
 ];
+
+
+exports.updatePostValidation = [
+  body("title")
+    .optional() // Only validate if provided
+    .trim()
+    .isLength({ min: 5 })
+    .withMessage("Title must be at least 5 characters long"),
+
+  body("content")
+    .optional() // Only validate if provided
+    .trim()
+    .isLength({ min: 5 })
+    .withMessage("Content must be at least 5 characters long"),
+];
