ae851ebd2227e4cdc1d96b215cb4b78cf6057a4e
Shittu Emmanuel <shem4soul@gmail.com>
2025-11-08 15:56:47 +0100

    added cloudinary

--- controllers/feed.js
+++ controllers/feed.js
@@ -23,13 +23,19 @@ exports.createPost = (req, res, next) => {
     const error = new Error("Validation failed, entered data is incorrect.");
     error.statusCode = 422;
     throw error;
-  }
+  } 
+    if (!req.file) {
+      const error = new Error('No image provided.');
+      error.statusCode = 422;
+      throw error;
+    }
+  const imageUrl = req.file.path;
   const title = req.body.title;
   const content = req.body.content;
   const post = new Post({
     title: title,
     content: content,
-    imageUrl: "images/hp.jpeg",
+    imageUrl: imageUrl,
     creator: { name: "shem" },
   });
   post
