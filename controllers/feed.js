788d29660e472b0c7fb79bd1db3b61336e6c9a83
Shittu Emmanuel <shem4soul@gmail.com>
2025-11-10 15:53:47 +0100

    added pagination

--- controllers/feed.js
+++ controllers/feed.js
@@ -4,11 +4,23 @@ const Post = require("../models/post");
 const cloudinary = require('../config/cloudinary'); 
 
 exports.getPosts = (req, res, next) => {
+  const currentPage = req.query.page || 1;
+  const perPage = 2;
+  let totalItems;
   Post.find()
+    .countDocuments()
+    .then((count) => {
+      totalItems = count;
+      return Post.find()
+        .skip((currentPage - 1) * perPage)
+        .limit(perPage);
+    })
     .then((posts) => {
-      res
-        .status(200)
-        .json({ message: "Fetched posts successfully.", posts: posts });
+      res.status(200).json({
+        message: "Fetched posts successfully.",
+        posts: posts,
+        totalItems: totalItems,
+      });
     })
     .catch((err) => {
       if (!err.statusCode) {
