5daeaa8118cc0943b7c570dcd896c871c0d59579
Shittu Emmanuel <shem4soul@gmail.com>
2025-11-11 16:56:58 +0100

    added validation for login in the routes file

--- routes/auth.js
+++ routes/auth.js
@@ -3,11 +3,14 @@ const express = require('express');
 const router = express.Router();
 const authController = require("../controllers/auth");
 
-const { signupValidation } = require("../validations/authValidation");
+const { signupValidation, loginValidation } = require("../validations/authValidation");
 
 
 
 
 router.post("/signup", signupValidation, authController.signup);
+
+
+router.post("/login", loginValidation, authController.login);
 
 module.exports = router;
