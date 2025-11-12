866ec87895b8d2fd0233c1dbb568ad47a2c8dbbc
Shittu Emmanuel <shem4soul@gmail.com>
2025-11-12 04:18:50 +0100

    added isAuth to the route for getting posts

--- routes/feed.js
+++ routes/feed.js
@@ -2,6 +2,8 @@ const express = require('express');
 
 const feedController = require('../controllers/feed');
 
+const isAuth = require('../middlewares/is-auth');
+
 const { createPostValidation, updatePostValidation } = require("../validations/feedValidation");
 
 const upload = require("../middlewares/multer");
@@ -9,7 +11,7 @@ const upload = require("../middlewares/multer");
 const router = express.Router();
 
 //GET/ feed/posts
-router.get('/posts', feedController.getPosts);
+router.get('/posts', isAuth, feedController.getPosts);
 
 //Post/ feed/post
 router.post('/post', upload.single('image'), createPostValidation, feedController.createPost);
