8fe2c065f3d298886855d457ccb0245d075c6f11
Shittu Emmanuel <shem4soul@gmail.com>
2025-11-12 18:37:48 +0100

    fix a bug

--- middlewares/is-auth.js
+++ middlewares/is-auth.js
@@ -7,7 +7,7 @@ module.exports = (req, res, next) => {
         error.statusCode = 401;
         throw error;
     }
-    const token = req.get('Authorization').split(' ')[1];
+    const token = authHeader.split(' ')[1];
     let decodedToken;
     try {
         decodedToken = jwt.verify(token,  process.env.JWT_SECRET);
